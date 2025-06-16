c97221098e6c1e6d3db7d6793b9d2e62c8dc8456
Grant Gainey <ggainey@redhat.com>
2025-06-16 15:40:23 -0400

    Files moved to allow a more-direct diff

--- code/wandsys.c
+++ code/wandsys.c
@@ -1,125 +1,100 @@
-#include    <stdio.h>
-#include    "wanddef.h"
-/*
-**      WANDSYS -- System dependent routines for Wander
-**	    Interfaces to stdio routines to keep track of file position
-**	    on V6 systems without tell().
-** Copyright (c) by Peter S. Langston - New  York,  N.Y.
+#include <stdio.h>
+#include <stdlib.h>
+#include "wanddef.h"
+
+/* WANDSYS -- System dependent routines for Wander
+** Interfaces to stdio routines to keep track of file position
+** Copyright (c) by Peter S. Langston - New York, N.Y.
 */
 
-/*#define	NOFTELL		/* uncomment this for systems without tell() */
+/* Uncomment next line for systems without ftell() */
+//#define NOFTELL
 
 #ifdef NOFTELL
-static	char    *whatwand = "@(#)wandsys.c	1.1  WITHOUT FTELL() 2/22/84 -- (c) psl";
+static const char *whatwand = "@(#)wandsys.c\t1.1  WITHOUT FTELL() 2/22/84 -- (c) psl";
 #else
-static	char    *whatwand = "@(#)wandsys.c	1.1  with ftell() 2/22/84 -- (c) psl";
+static const char *whatwand = "@(#)wandsys.c\t1.1  with ftell() 2/22/84 -- (c) psl";
 #endif
-static	char    *wand_h =   H_SCCS;
+static const char *wand_h = H_SCCS;
 
-extern	FILE    *fpungot;
+FILE *fpungot = NULL;
 
 #ifdef NOFTELL
-#define	NUMWTELL	2
-struct	wtellstr {
-	FILE	*wt_fp;				/* associated file pointer */
-	long	wt_addr;				/* wtell() address */
+#define NUMWTELL 2
+struct wtellstr {
+    FILE *wt_fp;
+    long wt_addr;
 } wt[NUMWTELL + 1];
 #endif
 
-FILE	*
-wopen(file, rwflg)			 	     /* interface to fopen */
-char	*file, *rwflg;
-{
-#ifdef	NOFTELL
-	register int i;
-	FILE *fp;
-
-	if ((fp = fopen(file, rwflg)) != (FILE *) NULL) {
-	    i = wtfind((FILE *) NULL);
-	    wt[i].wt_fp = fp;
-	    wt[i].wt_addr = 0L;
-	}
-	return(fp);
-#else
-	return(fopen(file, rwflg));
-#endif
-}
-
-wseek(fp, addr, mode)				   /* interface to fseek() */
-FILE	*fp;
-long	addr;
-{
+FILE *wopen(const char *file, const char *rwflg) {
 #ifdef NOFTELL
-	register int i;
-
-	i = wtfind(fp);
-	if (mode == 0)
-	    wt[i].wt_addr = addr;
-	else if (mode == 1)
-	    wt[i].wt_addr += addr;
-	else if (mode == 2)
-	    printf("Illegal wseek(%d, %D, %d)\n", fp, addr, mode);
-#endif
-	if (fpungot == fp)
-	    fpungot = (FILE *) NULL;
-	return(fseek(fp, addr, mode));
-}
-
-wgetc(fp)					    /* interface to getc() */
-FILE	*fp;
-{
-#ifdef	NOFTELL
-	register int i;
-
-	if (fp != stdin) {
-	    i = wtfind(fp);
-	    wt[i].wt_addr++;
-	}
-#endif
-	return(getc(fp));
-}
-
-long
-wtell(fp)
-FILE	*fp;
-{
-#ifdef	NOFTELL
-	register int i;
-
-	if (fp == stdin)
-	    return(0L);
-	i = wtfind(fp);
-	return(wt[i].wt_addr);
+    int i;
+    FILE *fp;
+    if ((fp = fopen(file, rwflg)) != NULL) {
+        i = wtfind(NULL);
+        wt[i].wt_fp = fp;
+        wt[i].wt_addr = 0L;
+    }
+    return fp;
 #else
-	extern long ftell();
-
-	return(ftell(fp));
+    return fopen(file, rwflg);continue wand2.c
 #endif
 }
 
-wclose(fp)					  /* interface to fclose() */
-FILE	*fp;
-{
-#ifdef	NOFTELL
-	register int i;
-
-	i = wtfind(fp);
+int wseek(FILE *fp, long addr, int mode) {
+#ifdef NOFTELL
+    int i = wtfind(fp);
+    if (mode == 0)
+        wt[i].wt_addr = addr;
+    else if (mode == 1)
+        wt[i].wt_addr += addr;
+    else if (mode == 2)
+        fprintf(stderr, "Illegal wseek(%p, %ld, %d)\n", (void*)fp, addr, mode);
 #endif
-	if (fpungot == fp)
-	    fpungot = (FILE *) NULL;
-	return(fclose(fp));
+    if (fpungot == fp)
+        fpungot = NULL;
+    return fseek(fp, addr, mode);
 }
 
-#ifdef	NOFTELL
-wtfind(fp)		 	 /* find record for specified file pointer */
-FILE	*fp;
-{
-	register int i;
+int wgetc(FILE *fp) {
+#ifdef NOFTELL
+    int i;
+    if (fp != stdin) {
+        i = wtfind(fp);
+        wt[i].wt_addr++;
+    }
+#endif
+    return getc(fp);
+}
 
-	for (i = 0; i < NUMWTELL; i++)
-	    if (fp == wt[i].wt_fp)
-		return(i);
-	printf("wtfind(%d): Couldn't find a record\n", fp);
-	return(NUMWTELL);
+long wtell(FILE *fp) {
+#ifdef NOFTELL
+    int i;
+    if (fp == stdin)
+        return 0L;
+    i = wtfind(fp);
+    return wt[i].wt_addr;
+#else
+    return ftell(fp);
+#endif
+}
+
+int wclose(FILE *fp) {
+#ifdef NOFTELL
+    int i = wtfind(fp);
+#endif
+    if (fpungot == fp)
+        fpungot = NULL;
+    return fclose(fp);
+}
+
+#ifdef NOFTELL
+int wtfind(FILE *fp) {
+    for (int i = 0; i < NUMWTELL; i++)
+        if (fp == wt[i].wt_fp)
+            return i;
+    fprintf(stderr, "wtfind(%p): Couldn't find a record\n", (void*)fp);
+    return NUMWTELL;
 }
 #endif
